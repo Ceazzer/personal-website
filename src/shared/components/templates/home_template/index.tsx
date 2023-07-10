22d730d9e1fd7f1ef80c2a2457d36df7a49490bd
Ceazzer <kareem@rocketpunch.dev>
2023-07-10 08:35:14 -0600

    added type library to handle auto typings

--- src/shared/components/templates/home_template/index.tsx
+++ src/shared/components/templates/home_template/index.tsx
@@ -1,12 +1,18 @@
-import { HomeTemplateContainer } from "./styles";
+import { Parallax } from "react-parallax";
+import { BottomSeparator, HomeTemplateContainer } from "./styles";
 
 interface IHomeTemplateProps {
     children: React.ReactNode // TODO: change to support limited types
 }
 export function HomeTemplate(props: IHomeTemplateProps) {
     return (
-        <HomeTemplateContainer>
-            {props.children}
-        </HomeTemplateContainer>
+        <Parallax strength={500}>
+            <HomeTemplateContainer>
+                {props.children}
+                <BottomSeparator>
+                    <p>Separator</p>
+                </BottomSeparator>
+            </HomeTemplateContainer>
+        </Parallax>
     );
 }
--- src/shared/components/templates/home_template/styles.ts
+++ src/shared/components/templates/home_template/styles.ts
@@ -7,6 +7,16 @@ export const HomeTemplateContainer = tw.div`
     justify-center
     items-center
     min-w-full
-    min-h-[800px]
+    min-h-[95vh]
     bg-gray-100
 `;
+
+export const BottomSeparator = tw.div`
+    absolute
+    bottom-0
+    w-full
+    min-h-[80px]
+    bg-slate-500
+`;
+
+// TODO: Will need to add a png of me
