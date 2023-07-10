fb6d01395f5c6574932bbd9c5112bab7422a0a63
Ceazzer <kareem@rocketpunch.dev>
2023-07-10 05:04:07 -0600

    add parallax lib

--- src/shared/components/templates/home_template/index.tsx
+++ src/shared/components/templates/home_template/index.tsx
@@ -7,7 +7,6 @@ export function HomeTemplate(props: IHomeTemplateProps) {
     return (
         <HomeTemplateContainer>
             {props.children}
-            test
         </HomeTemplateContainer>
     );
 }
